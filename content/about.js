ad6faa9d9afbf57deaa27cb5e3fe281d8d4b8529
Hannah VanWingen-Eckertova <hannahvanova@Hannahs-MacBook-Pro-2.local>
2024-04-10 13:11:26 -0400

    add content

---
+++ content/about.js
@@ -0,0 +1,21 @@
+export const name = `Hannah VanWingen-Eckertova`;
+export const title = `Data Visualization Engineer & Frontend Developer`;
+export const bio = `Hi! I design and build interactive visual tools for the web.`
+export const about = {
+  paragraphs: [
+    `Welcome to my website ! Over the past 5 years I've gained professional expertise in data visualization, software engineering, and product development. Most recently As a data engineer, 
+        I led a frontend data visualization and metric reporting project, collaborated on building ETL pipelines, and supported analytics teams with database transformations. 
+       From research and design to development and testing, I'm committed to empowering individuals by crafting narratives and experiences that inform 
+        nuanced understanding and growth through accessible engagement with open source data.`,
+    `My main focus these days, besides the re-design and development of my personal website, is an investigation into the data on mobility in Detroit and Chicago's
+        densest neighborhoods. When I'm not at my keyboard, I enjoy fiber-related arts, volunteering with young learners, maintaining my community compost, and going on long 
+        walks with my dog.`,
+  ]
+}
+export const contactBlurb = `Feel free to reach out for any reason and I will happily be in touch at my earliest convenience.`
+export const links = {
+  linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-eckertova-248976115/',
+  github: 'https://github.com/hvanova',
+  observable: 'https://observablehq.com/user/@hannahvanova',
+  email: 'mailto:hannahvanova@gmail.com'
+}
---
+++ content/experience.js
@@ -0,0 +1,68 @@
+export default [
+    {
+        company: "Lark Health Technologies",
+        location: "Remote",
+        link: "https://www.lark.com/",
+        roles: [
+            {
+                title: "Software Engineer II, Data Engineering and Visualization",
+                range: "June - December 2023",
+                bullets: [
+                    `Designed and implemented data parsing, transformation, and quality assurance pipelines using Scala and Spark.`,
+                    `Initiated data tracking using the Soda data quality platform to enhance observability and bolster data reliability.`,
+                    `Optimized complex data processing workflows, ensuring timely delivery while minimizing resource utilization.`
+                ]
+            },
+            {
+                title: "Software Engineer I, Data Engineering and Visualization",
+                range: "January 2022 - June 2023",
+                bullets: [
+                    `Led the development of a configurable and automated data reporting service in React`,
+                    `Collaborated with cross-functional teams to design and implement data visualizations using D3.js.`,
+                    `Demonstrated commitment to ensuring reliable data systems performance by participating in on-call responsibilities using Opsgenie.`,
+                    `Documented product requirements and metric calculation details and to maintain data integrity and capture best practices for further development and addressing common issues.`,
+                    `Collaborated on data transformation components for a fully automated backend and frontend data reporting service.`
+                ]
+            }
+        ],
+        skills: [
+            `Javascript`, `Typescript`, `D3.js`, `React`, `Next.js`, `Scala`, `Spark`, `Databricks`, `Snowflake`, `SQL`
+        ]
+    },
+    {
+        company: `Michigan Aerospace Corporation`,
+        location: `Ann Arbor, MI`,
+        link: `https://www.michiganaerospace.com/`,
+        roles: [
+            {
+                title: `Research Scientist, Data Visualization`,
+                range: `July 2019 - June 2020`,
+                bullets: [
+                    `Led the development of a highly interactive and configurable D3.js data visualization library that enables users to explore complex datasets for time series, confusion matrices, stacked bar graphs, 2D Gaussian distributions, and network`,
+                    `Wrote a grant proposal to the US Department of Energy for a data sharing platform that automates the processing of raw data files by detecting data types and displaying an optimal visualization to showcase trends, correlations, distributions, and anomalies`,
+                    `Collaborated on data analytics and visualization for the ARGOS.ai project, a holistic automated platform that utilizes machine learning and drones to monitor and assess invasive plant species.`,
+                    `TODO add MORE`
+                ]
+            },
+        ],
+        skills: [
+            `Python`, `Javascript`, `D3.js`, `Vue.js`, `React`,
+        ]
+    },
+    {
+        company: `University of Michigan Digital Projects Studio`,
+        location: `Ann Arbor, MI`,
+        link: `https://www.michiganaerospace.com/`,
+        roles: [
+            {
+                title: `Research Scientist, Data Visualization`,
+                range: `July 2019 - June 2020`,
+                bullets: [
+                    `Led the development of a highly interactive and configurable D3.js data visualization library that enables users to explore complex datasets for time series, confusion matrices, stacked bar graphs, 2D Gaussian distributions, and network`,
+                    `Wrote a grant proposal to the US Department of Energy for a data sharing platform that automates the processing of raw data files by detecting data types and displaying an optimal visualization to showcase trends, correlations, distributions, and anomalies`,
+                    `Collaborated on data analytics and visualization for the ARGOS.ai project, a holistic automated platform that utilizes machine learning and drones to monitor and assess invasive plant species.`
+                ]
+            },
+        ]
+    }
+];
