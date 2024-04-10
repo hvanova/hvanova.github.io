d429dfe7464f7e01321df1e4931292c7dae43eb8
Hannah VanWingen-Eckertova <hannahvanova@Hannahs-MacBook-Pro-2.local>
2024-04-10 13:20:32 -0400

    revamp init

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,49 @@
 <script>
-  import Header from "./lib/components/Header.svelte";
-  import Projects from "./lib/components/Projects.svelte";
-  import About from "./lib/components/About.svelte";
-  import Contact from "./lib/components/Contact.svelte";
+  import Header from "./components/Header.svelte";
+  import Nav from "./components/Nav.svelte";
+  import About from "./components/About.svelte";
 </script>
 
-<main data-theme="garden" class="pb-10">
-  <Header />
-  <Projects />
-  <About />
-  <Contact />
+<main class="main bg-main font-inter">
+  <div class="col">
+    <Header />
+    <Nav />
+  </div>
+  <div class="col">
+    <About />
+  </div>
 </main>
 
-<style lang="stylus">
-  :global(html) {
-    background: rgb(20, 85, 100);
+<style>
+  .main {
+    background-position: center;
+    padding: 80px 160px;
+    height: 100vh;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    box-sizing: border-box; /* Ensure padding is included in the width */
+  }
+  .main-body {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px; /* Add padding between the columns */
+    box-sizing: border-box; /* Ensure padding is included in the width */
+  }
+  .col {
+    flex: 1;
+    flex-direction: column;
+    display: flex;
+    min-width: 300px;
+    box-sizing: border-box;
+  }
+  @media screen and (max-width: 768px) {
+    .main {
+      padding: 20px;
+    }
+    .col {
+      width: 100%;
+      padding: 20px;
+    }
   }
 </style>
