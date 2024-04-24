b721b0a665e4a8ac22e102e9086069da5774119a
Hannah VanWingen-Eckertova <hannahvanova@Hannahs-MacBook-Pro-2.local>
2024-04-24 10:21:25 -0400

    update, fix typos

--- content/about.js
+++ content/about.js
@@ -2,12 +2,12 @@ export const name = `Hannah VanWingen-Eckertova`;
 export const title = `Data Visualization Engineer & Frontend Developer`;
 
 export const intro = `From research and design to development and testing, I work to empower individuals by building narratives and experiences that foster nuanced understanding and growth via accessible and interactive data engagement.`
-export const aboutP1 = `Over the past 5 years, I have worked in the data visualization field, including design and storytelling, data cleaning and preparation, statistical techniques for data exploration, cross-team communications, product development, frontend engineering, and ETL pipelines.
-    My interest in social and accessible design drives my work forward in a way that necessitates research, a part of my work that I will always value and prioritize.`
+export const aboutP1 = `Over the past 5 years, I have held a variety of roles within the field of data visualization. My professional experience includes visualization design and storytelling, data cleaning and preparation, statistical techniques for data exploration, project management, product development, frontend engineering, and backend engineering.
+    My interest in social design drives my work forward in a way that necessitates research, a practice I will always value and prioritize.`
 export const aboutP2 =
-  `My main focus these days, besides the re-design and development of my personal website, is a data investigation regarding Detroit and Chicago's
-        densest neighborhoods. Searching through urban development and mobility data, I aim to tell stories and expand literacy around around gentrification and displacement. 
-        When I'm not at my keyboard, I enjoy volunteering with young learners, fiber-related arts, working in my community gardens, and going on long 
+  `My primary focus these days, besides the re-design and development of my personal website, is a data investigation regarding Detroit and Chicago's
+        densest neighborhoods. Looking at urban development and mobility data, I aim to tell stories and expand literacy around gentrification and displacement. 
+        When I'm not at my keyboard, I enjoy volunteering with young learners, fiber-related arts, working in my community gardens, and long 
         walks with my dog.`
 
 export const links = {
