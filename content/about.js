3d062183340d8e696b9d76791f57bda7f0cdf0d3
Hannah VanWingen-Eckertova <hannahvanova@Hannahs-MacBook-Pro-2.local>
2024-05-17 10:50:52 -0400

    update

--- content/about.js
+++ content/about.js
@@ -3,7 +3,7 @@ export const title = `Software Engineer, Data & Visualization`;
 
 export const intro = `From research and design to development and testing, I craft nuanced understanding and tell powerful stories that inspire action through accessible and interactive data engagement.`
 export const aboutP1 = `Over the past 5 years, I've gained multidisciplinary experiences in analyzing, engineering, and visualizing data. My professional work includes visualization design and storytelling, data cleaning and preparation, statistical techniques for data exploration, project management, product development, and full-stack engineering.
-    My interest in socially informed design drives my work forward in a way that necessitates continued research, a practice I will always value and prioritize.`
+My interest in social design, authentic storytelling, and optimized solutions necessitate research as a valuable part of my approach to collaboration in my work.`
 export const aboutP2 =
   `My primary focus these days, besides the re-design and development of my personal website, is a data investigation regarding Detroit and Chicago's
         densest neighborhoods. Looking at urban development and mobility data, I aim to tell stories and grow awareness concerning gentrification and displacement. 
