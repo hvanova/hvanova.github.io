88b972678417545a8da97b530b0ca823c0bc82d1
Hannah VanWingen-Eckertova <hannahvanova@Hannahs-MacBook-Pro-2.local>
2024-06-05 18:31:09 -0400

    resume line update

--- content/about.js
+++ content/about.js
@@ -1,14 +1,12 @@
 export const name = `Hannah VanWingen-Eckertova`;
 export const title = `Software Engineer, Data & Visualization`;
 
-export const intro = `From research and design to development and testing, I craft nuanced understanding and tell powerful stories that inspire action through accessible and interactive data engagement.`
-export const aboutP1 = `Over the past 5 years, I've gained multidisciplinary experiences in analyzing, engineering, and visualizing data. My professional work includes visualization design and storytelling, data cleaning and preparation, statistical techniques for data exploration, project management, product development, and full-stack engineering.
-My interest in social design, authentic storytelling, and optimized solutions necessitate research as a valuable part of my approach to collaboration in my work.`
-export const aboutP2 =
-  `My primary focus these days, besides the re-design and development of my personal website, is a data investigation regarding Detroit and Chicago's
-        densest neighborhoods. Looking at urban development and mobility data, I aim to tell stories and grow awareness concerning gentrification and displacement. 
-        When I'm not at my keyboard, I enjoy volunteering with young learners, fiber-related arts, working in my community gardens, and long 
-        walks with my dog.`
+export const intro = `From research and design to development and testing, I craft a nuanced understanding of software capabilities to support human well-being, tell impactful stories, and inspire equitable growth. 
+  Experienced in both academic and corporate environments, I am seeking roles in engineering and analytics where development is socially and statistically informed.`
+export const aboutP1 = `Over the past 5 years, I've gained multidisciplinary experience in data analysis, engineering, and visualization. 
+  My work spans visualization design, data preparation, statistical analysis, project management, product development, and full-stack engineering. My passion for social design, authentic storytelling, and optimized solutions makes research a vital part of my collaborative approach.`
+export const aboutP2 = `These days, my main focus is investigating data on the densest neighborhoods, analyzing urban development and mobility metrics to tell stories and raise awareness about gentrification and displacement. 
+  When I'm not at my keyboard, I enjoy volunteering with young learners, fiber-related arts, working in my community gardens, and long walks with my dog.`
 
 export const links = {
   linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-eckertova-248976115/',
