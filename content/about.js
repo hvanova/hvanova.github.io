96cbc710de550be3e8547d4207c555e402533a60
Hannah VanWingen-Eckertova <hannahvanova@Hannahs-MacBook-Pro-2.local>
2024-06-10 12:14:45 -0400

    update

--- content/about.js
+++ content/about.js
@@ -1,8 +1,8 @@
 export const name = `Hannah VanWingen-Eckertova`;
 export const title = `Software Engineer, Data & Visualization`;
 
-export const intro = `From research and design to development and testing, I craft a nuanced understanding of software capabilities to support human well-being, tell impactful stories, and inspire equitable growth. 
-  Experienced in both academic and corporate environments, I am seeking roles in engineering and analytics where development is socially and statistically informed.`
+export const intro = `From research and design to development and testing, I advance software capabilities to support human well-being and tell impactful stories. 
+With experience in both educational and business environments, I seek socially and statistically informed projects that promote equitable and progressive growth.`
 export const aboutP1 = `Over the past 5 years, I've gained multidisciplinary experience in data analysis, engineering, and visualization. 
   My work spans visualization design, data preparation, statistical analysis, project management, product development, and full-stack engineering. My passion for social design, authentic storytelling, and optimized solutions makes research a vital part of my collaborative approach.`
 export const aboutP2 = `These days, my main focus is investigating data on the densest neighborhoods, analyzing urban development and mobility metrics to tell stories and raise awareness about gentrification and displacement. 
