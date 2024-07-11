0fae6e0a9c16db35724a115439ba5dca7983dfa0
Hannah VanWingen-Eckertova <hannahvanova@Hannahs-MacBook-Pro-2.local>
2024-07-11 16:31:06 -0400

    content update

--- content/about.js
+++ content/about.js
@@ -1,12 +1,12 @@
 export const name = `Hannah VanWingen-Eckertova`;
 export const title = `Software Engineer, Data & Visualization`;
 
-export const intro = `From research and design to development and testing, I advance software capabilities to support human well-being and tell impactful stories. 
-With experience in both educational and business environments, I seek socially and statistically informed projects that promote equitable and progressive growth.`
+export const intro = `From research and design to development and testing, I advance software capabilities to support impact-driven initiatives. 
+  With experience working in academic and corporate environments, I leverage statistically informed techniques and accessible visual engagement methods to communicate data-driven stories.`
 export const aboutP1 = `Over the past 5 years, I've gained multidisciplinary experience in data analysis, engineering, and visualization. 
-  My work spans visualization design, data preparation, statistical analysis, project management, product development, and full-stack engineering. My passion for social design, authentic storytelling, and optimized solutions makes research a vital part of my collaborative approach.`
-export const aboutP2 = `These days, my main focus is investigating data on the densest neighborhoods, analyzing urban development and mobility metrics to tell stories and raise awareness about gentrification and displacement. 
-  When I'm not at my keyboard, I enjoy volunteering with young learners, fiber-related arts, working in my community gardens, and long walks with my dog.`
+  My work spans visualization design, statistical analysis, project management, product development, and full-stack engineering. My experience in research has been instrumental in shaping my multidisciplinary expertise into a career focused on problem-solving and collaborative efforts.`
+export const aboutP2 = `In my professional and personal work, I am passionate about data projects that articulate important stories and insights through informed statistical transformations and comprehensive visual displays. 
+  When I'm not at my keyboard, I enjoy volunteering with young learners, fiber-related arts, and long walks with my dog.`
 
 export const links = {
   linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-eckertova-248976115/',
