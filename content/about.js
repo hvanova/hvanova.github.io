4209a0a3082b44b60e1729a8a2b953633246a57f
Hannah VanWingen-Eckertova <your-email@example.com>
2024-07-14 23:08:35 -0400

    content update

--- content/about.js
+++ content/about.js
@@ -1,12 +1,12 @@
 export const name = `Hannah VanWingen-Eckertova`;
-export const title = `Software Engineer, Data & Visualization`;
+export const title = `Software Engineer, Data Analytics & Visualization`;
 
-export const intro = `From research and design to development and testing, I advance software capabilities to support impact-driven initiatives. 
-  With experience working in academic and corporate environments, I leverage statistically informed techniques and accessible visual engagement methods to communicate data-driven stories.`
-export const aboutP1 = `Over the past 5 years, I've developed a multidisciplinary career in data analysis, engineering, and visualization. 
-  My early experience in research has been instrumental in guiding my work toward design-thinking efforts with a focus on communicating data insights for nuanced understanding and impact.`
-export const aboutP2 = `In my professional and personal work, I’m passionate about data projects that collect and translate complex phenomena into prescriptive solutions. 
-  I am especially motivated by the intersection of data and advocacy, where the visual expression of data can be applied to shift perception and inform action.`
+export const intro = `Hi! I build software to drive impact, transforming complex dynamics into actionable insights.
+  Leveraging statistically informed techniques and accessible graphical displays, I bring a computational approach to data-driven storytelling.`
+export const aboutP1 = `Over the past 5 years, I supported technical initiatives related to data strategy, management, tool development, and analytics. My projects included data transformation and modeling, algorithm design and implementation, applications of generative machine learning functions, pipeline development, API integrations, and interactive data visualization. 
+From research and design to development and testing, I am passionate about applying the latest tools to develop innovative solutions. My experience in research has been instrumental in shaping my multidisciplinary expertise into a technical career focused on design thinking and collaborative efforts.`
+export const aboutP2 = `I thrive in environments encouraging deep examinations, creative problem-solving, and continued learning.
+  I am especially motivated by the intersection of data and advocacy, where data visualization can shift perception and inform action.`
 
 export const links = {
   linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-eckertova-248976115/',
