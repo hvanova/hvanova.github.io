398e868267c4fd0c02ae5eb376950a3f6ad86c2c
Hannah VanWingen-Eckertova <your-email@example.com>
2024-07-16 15:18:20 -0400

    update content

--- content/about.js
+++ content/about.js
@@ -1,12 +1,13 @@
 export const name = `Hannah VanWingen-Eckertova`;
 export const title = `Software Engineer, Data Analytics & Visualization`;
 
-export const intro = `Hi! I build software to drive impact, transforming complex dynamics into actionable insights.
+export const intro = `Hi!  I create technical solutions that transform complex dynamics into actionable insights.
   Leveraging statistically informed techniques and accessible graphical displays, I bring a computational approach to data-driven storytelling.`
-export const aboutP1 = `Over the past 5 years, I supported technical initiatives related to data strategy, management, tool development, and analytics. My projects included data transformation and modeling, algorithm design and implementation, applications of generative machine learning functions, pipeline development, API integrations, and interactive data visualization. 
-From research and design to development and testing, I am passionate about applying the latest tools to develop innovative solutions. My experience in research has been instrumental in shaping my multidisciplinary expertise into a technical career focused on design thinking and collaborative efforts.`
-export const aboutP2 = `I thrive in environments encouraging deep examinations, creative problem-solving, and continued learning.
-  I am especially motivated by the intersection of data and advocacy, where data visualization can shift perception and inform action.`
+export const aboutP1 = `My multidisciplinary expertise has shaped a technical career focused on collaborative design thinking and ambitious performance goals. 
+  Over the past 5 years, I have supported and led various technical initiatives in data strategy and management, analytics, and full-stack development.
+  From research and design to development and testing, I am passionate about applying the latest tools to develop innovative solutions.`
+export const aboutP2 = `I excel in environments encouraging deep examinations, creative problem-solving, and continuous learning. 
+  I am particularly motivated by the intersection of data and advocacy, aiming to contribute to collective good and equitable reform.`
 
 export const links = {
   linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-eckertova-248976115/',
