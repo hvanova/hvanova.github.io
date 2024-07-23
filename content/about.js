1a4c43e67dfa2b49bafc7f7b1c7862405b7cff7d
Hannah VanWingen-Eckertova <your-email@example.com>
2024-07-23 12:07:52 -0400

    content update

--- content/about.js
+++ content/about.js
@@ -1,13 +1,10 @@
 export const name = `Hannah VanWingen-Eckertova`;
-export const title = `Software Engineer, Data Analytics & Visualization`;
+export const title = `Software Engineer, Data & Full-Stack`;
 
-export const intro = `Hi!  I create technical solutions that transform complex dynamics into actionable insights.
-  Leveraging statistically informed techniques and accessible graphical displays, I bring a computational approach to data-driven storytelling.`
-export const aboutP1 = `My multidisciplinary expertise has shaped a technical career focused on collaborative design thinking and ambitious performance goals. 
-  Over the past 5 years, I have supported and led various technical initiatives in data strategy and management, analytics, and full-stack development.
-  From research and design to development and testing, I am passionate about applying the latest tools to develop innovative solutions.`
-export const aboutP2 = `I excel in environments encouraging deep examinations, creative problem-solving, and continuous learning. 
-  I am particularly motivated by the intersection of data and advocacy, aiming to contribute to collective good and equitable reform.`
+export const intro = `Committed to sustainable and equitable growth, I create robust technical solutions and engaging user experiences for strategic action.`
+export const aboutP1 = `My multidisciplinary expertise has cultivated a career focused on collaborative design thinking, innovative problem-solving, scalable implementations, and rigorous quality assurance. 
+  Over the past five years, I have supported and led various technical initiatives in data strategy and management, analytics, and full-stack development.
+  From research and design to development and testing, I am excited to leverage cutting-edge technology to develop reliable and accessible solutions.`
 
 export const links = {
   linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-eckertova-248976115/',
