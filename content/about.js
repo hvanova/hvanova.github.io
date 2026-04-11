581f4f592c45fc788d6c622b1f61e3064485d10e
hannah vanwingen-eckertova <hvanova@umich.edu>
2026-04-11 01:23:20 -0400

    Update about.js

--- content/about.js
+++ content/about.js
@@ -1,12 +1,16 @@
-export const name = `Hannah VanWingen-Eckertova`;
-export const title = `Software Engineer – Data & Visualization`;
-
-export const intro = `My work centers on developing unbiased analyses and adaptive solutions, prioritizing interdisciplinary collaboration to optimize innovation for accessibility and impact.`
-export const aboutP1 = `As a software engineer, I leverage creative and technical skills to support effective decision-making, from data warehousing to web systems and visualization. With an academic background in physics and complex systems, I apply design thinking, research-driven methods, and advanced computational techniques to build insightful tools and systems.`
-
+export const name = `Hannah VanWingen Eckertova`;
+export const title = `Data Scientist | Research`;
+export const bio = `global health + clean energy + sustainable design`
+export const about = {
+  paragraphs: [
+    `In my free time, I use secondhand and post-consumer materials to explore themes of circularity, resiliency, and nostalgia in fiber craft. \
+    Someday I'll share my art here! For now, please enjoy this elevated resume.`
+  ]
+}
+export const contactBlurb = `Feel free to reach out for any reason and I will happily be in touch at my earliest convenience.`
 export const links = {
-  linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-eckertova-248976115/',
+  linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-248976115/',
   github: 'https://github.com/hvanova',
   observable: 'https://observablehq.com/user/@hannahvanova',
-  email: 'mailto:hannahvanova@gmail.com'
+  email: 'mailto:hannahvanwingena@gmail.com'
 }
