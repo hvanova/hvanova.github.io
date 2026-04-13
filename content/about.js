c02b55706345602fc8754b26d0ff7438b347e689
hannah vanwingen-eckertova <hannahvanwingen@gmail.com>
2026-04-12 21:31:16 -0400

    Update about.js

--- content/about.js
+++ content/about.js
@@ -1,12 +1,8 @@
 export const name = `Hannah VanWingen Eckertova`;
 export const title = `Data Scientist | Research`;
 export const bio = `global health + clean energy + sustainable design`
-export const about = {
-  paragraphs: [
-    `In my free time, I use secondhand and post-consumer materials to explore themes of circularity, resiliency, and nostalgia in fiber craft. \
+export const about = `In my free time, I use secondhand and post-consumer materials to explore themes of circularity, resiliency, and nostalgia in fiber craft. \
     Someday I'll share my art here! For now, please enjoy this elevated resume.`
-  ]
-}
 export const contactBlurb = `Feel free to reach out for any reason and I will happily be in touch at my earliest convenience.`
 export const links = {
   linkedin: 'https://www.linkedin.com/in/hannah-vanwingen-248976115/',
