45431589240f88f97c1a85279bb5a732e7569534
hannah vanwingen eckertova <hannahvanwingen@gmail.com>
2026-07-23 22:50:23 -0400

    Update about.js

--- content/about.js
+++ content/about.js
@@ -1,5 +1,5 @@
 export const name = `Hannah VanWingen Eckertova`;
-export const title = `Data Scientist | Research`;
+export const title = `Systems Analyst | Data Scientist`;
 export const bio = `Learning and working at the intersection of global health and sustainable design.`
 export const about = `I'm currently digging into clean energy, affordable housing, and my garden. I also use secondhand and post-consumer materials to explore themes of nostalgia and resiliency in fiber craft. \
     Someday I'll share my art here! For now, please enjoy this elevated resume.`
